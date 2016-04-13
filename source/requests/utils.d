08d7617f54293358145c5a1c1441bb5becf09ae1
Igor Khasilev <igor.khasilev@gmail.com>
2016-04-13 20:44:06 +0300

    rename

---
+++ source/requests/utils.d
@@ -0,0 +1,26 @@
+module requests.utils;
+
+static immutable short[string] standard_ports;
+static this() {
+    standard_ports["http"] = 80;
+    standard_ports["https"] = 443;
+}
+
+
+auto getter(string name) {
+    return `
+        @property auto ` ~ name ~ `() const @safe @nogc {
+            return __` ~ name ~ `;
+        }
+    `;
+}
+auto setter(string name) {
+    string member = "__" ~ name;
+    string t = "typeof(this."~member~")";
+    return `
+        @property void ` ~ name ~`(` ~ t ~ ` s) {`~ 
+    member ~`=s;
+        }
+    `;
+}
+

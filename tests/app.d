a0ee009efc84a7894c1b594f677e1c91fcd3c3d3
Igor Khasilev <igor.khasilev@gmail.com>
2016-07-20 16:28:47 +0300

    correct version for httpbin app test

--- tests/app.d
+++ tests/app.d
@@ -1,14 +1,18 @@
 import httpbin;
 import requests;
 
-unittest {
-    auto s = httpbinApp();
-    scope (exit) {
-        s.stop();
+version(vibeD) {
+}
+else {
+    unittest {
+        auto s = httpbinApp();
+        scope (exit) {
+            s.stop();
+        }
+        s.start();
+        auto rq = Request();
+        rq.keepAlive = false;
+        auto rs = rq.get("http://localhost:8081/get");
+        assert(rs.code == 200);
     }
-    s.start();
-    auto rq = Request();
-    rq.keepAlive = false;
-    auto rs = rq.get("http://localhost:8081/get");
-    assert(rs.code == 200);
 }

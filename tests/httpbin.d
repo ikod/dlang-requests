b0230644f3126eeb0ebf9b03c9780d1d971cd1db
Igor Khasilev <igor.khasilev@gmail.com>
2016-07-31 18:43:23 +0300

    switch to RequestArgs in handler; version=httpbin required to compile httpbin server

--- tests/httpbin.d
+++ tests/httpbin.d
@@ -2,7 +2,8 @@ module httpbin;
 
 version(vibeD) {
 } else
-version (unittest) {
+version (httpbin) 
+{
 	import requests.server;
 	import requests.utils;
 	import std.datetime;
@@ -11,6 +12,7 @@ version (unittest) {
 	import std.range;
 	import std.string;
 	import core.thread;
+	import std.experimental.logger;
 
 	auto buildReply(ref HTTPD_Request rq) {
 		auto method  = JSONValue(rq.method);
@@ -36,28 +38,29 @@ version (unittest) {
 
 	HTTPD httpbinApp() {
 	    pragma(msg, "Compiling httpbin server");
-
+		debug(requests) trace("start httpbin app");
 		HTTPD server = new HTTPD();
 		App httpbin = App("httpbin");
 		
 		httpbin.port = 8081;
 		httpbin.host = "0.0.0.0"; 
 		httpbin.timeout = 10.seconds;
+		httpbin.rqLimit = 5;
 		server.app(httpbin);
 
-		auto root(in App app, ref HTTPD_Request rq, Route ro) {
+		auto root(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			debug (httpd) trace("handler / called");
 			auto rs = response(rq, buildReply(rq));
 			rs.headers["Content-Type"] = "application/json";
 			return rs;
 		}
-		auto get(in App app, ref HTTPD_Request rq, Route ro) {
+		auto get(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			debug (httpd) trace("handler /get called");
 			auto rs = response(rq, buildReply(rq));
 			rs.headers["Content-Type"] = "application/json";
 			return rs;
 		}
-		auto del(in App app, ref HTTPD_Request rq, Route ro) {
+		auto del(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			if ( rq.method != "DELETE") {
 				auto rs = response(rq, "Illegal method %s".format(rq.method), 405);
 				return rs;
@@ -67,7 +70,7 @@ version (unittest) {
 				return rs;
 			}
 		}
-		auto put(in App app, ref HTTPD_Request rq, Route ro) {
+		auto put(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			if ( rq.method != "PUT") {
 				auto rs = response(rq, "Illegal method %s".format(rq.method), 405);
 				return rs;
@@ -77,7 +80,7 @@ version (unittest) {
 				return rs;
 			}
 		}
-		auto patch(in App app, ref HTTPD_Request rq, Route ro) {
+		auto patch(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			if ( rq.method != "PATCH") {
 				auto rs = response(rq, "Illegal method %s".format(rq.method), 405);
 				return rs;
@@ -87,26 +90,26 @@ version (unittest) {
 				return rs;
 			}
 		}
-		auto post(in App app, ref HTTPD_Request rq, Route ro) {
+		auto post(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto rs = response(rq, buildReply(rq));
 			return rs;
 		}
-		auto gzip(in App app, ref HTTPD_Request rq, Route ro) {
+		auto gzip(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto content = ["gzipped":true];
 			auto rs = response(rq, JSONValue(content).toPrettyString);
 			rs.compress(Compression.gzip);
 			rs.headers["Content-Type"] = "application/json";
 			return rs;
 		}
-		auto deflate(in App app, ref HTTPD_Request rq, Route ro) {
+		auto deflate(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto content = ["deflated":true];
 			auto rs = response(rq, JSONValue(content).toPrettyString);
 			rs.compress(Compression.deflate);
 			return rs;
 		}
-		auto rel_redir(in App app, ref HTTPD_Request rq, Route ro) {
+		auto rel_redir(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto rs = response(rq, buildReply(rq));
-			auto redirects = to!long(ro.captures["redirects"]);
+			auto redirects = to!long(args["redirects"]);
 			if ( redirects > 1 ) {
 				rs.headers["Location"] = "/relative-redirect/%d".format(redirects-1);
 			} else {
@@ -115,18 +118,18 @@ version (unittest) {
 			rs.status    = 302;
 			return rs;
 		}
-		auto abs_redir(in App app, ref HTTPD_Request rq, Route ro) {
+		auto abs_redir(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto rs = response(rq, buildReply(rq));
-			auto redirects = to!long(ro.captures["redirects"]);
+			auto redirects = to!long(args["redirects"]);
 			if ( redirects > 1 ) {
-				rs.headers["Location"] = "http://localhost:8081/absolute-redirect/%d".format(redirects-1);
+				rs.headers["Location"] = "http://0.0.0.0:8081/absolute-redirect/%d".format(redirects-1);
 			} else {
-				rs.headers["Location"] = "http://localhost:8081/get";
+				rs.headers["Location"] = "http://0.0.0.0:8081/get";
 			}
 			rs.status    = 302;
 			return rs;
 		}
-		auto cookiesSet(in App app, ref HTTPD_Request rq, Route ro) {
+		auto cookiesSet(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			Cookie[] cookies;
 			foreach(p; rq.query.byKeyValue) {
 				cookies ~= Cookie("/cookies", rq.requestHeaders["host"], p.key, p.value);
@@ -136,21 +139,21 @@ version (unittest) {
 			rs.cookies = cookies;
 			return rs;
 		}
-		auto cookies(in App app, ref HTTPD_Request rq, Route ro) {
+		auto cookies(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			auto cookies = ["cookies": JSONValue(rq.cookies)];
 			auto rs = response(rq, JSONValue(cookies).toString);
 			return rs;
 		}
-		auto range(in App app, ref HTTPD_Request rq, Route ro) {
-			auto size = to!long(ro.captures["size"]);
+		auto range(in App app, ref HTTPD_Request rq, RequestArgs args) {
+			auto size = to!long(args["size"]);
 			auto rs = response(rq, new ubyte[size].chunks(16));
 			rs.compress(Compression.yes);
 			return rs;
 		}
-		auto basicAuth(in App app, ref HTTPD_Request rq, Route ro) {
+		auto basicAuth(in App app, ref HTTPD_Request rq, RequestArgs args) {
 			import std.base64;
-			auto user    = ro.captures["user"];
-			auto password= ro.captures["password"];
+			auto user    = args["user"];
+			auto password= args["password"];
 			auto auth    = cast(string)Base64.decode(rq.requestHeaders["authorization"].split()[1]);
 			auto up      = auth.split(":");
 			short status;
@@ -163,15 +166,16 @@ version (unittest) {
 			rs.headers["Content-Type"] = "application/json";
 			return rs;
 		}
-		auto delay(in App app, ref HTTPD_Request rq, Route ro) {
-			auto delay = dur!"seconds"(to!long(ro.captures["delay"]));
+		auto delay(in App app, ref HTTPD_Request rq, RequestArgs args) {
+			auto delay = dur!"seconds"(to!long(args["delay"]));
 			Thread.sleep(delay);
 			auto rs = response(rq, buildReply(rq));
 			rs.headers["Content-Type"] = "application/json";
 			return rs;
 		}
-		auto stream(in App app, ref HTTPD_Request rq, Route ro) {
-			auto lines = to!long(ro.captures["lines"]);
+		auto stream(in App app, ref HTTPD_Request rq, RequestArgs args) {
+			auto lines = to!long(args["lines"]);
+			import std.stdio;
 			auto rs = response(rq, (buildReply(rq) ~ "\n").repeat(lines));
 			rs.headers["Content-Type"] = "application/json";
 			return rs;

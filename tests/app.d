953922d185c10b4dda04cd319b3040e5e586256a
Igor Khasilev <igor.khasilev@gmail.com>
2016-07-31 18:44:52 +0300

    switch to RequestArgs in httpd handlers

--- tests/app.d
+++ tests/app.d
@@ -14,7 +14,7 @@ else {
         s.start();
         auto rq = Request();
         rq.keepAlive = false;
-        auto rs = rq.get("http://localhost:8081/get");
+        auto rs = rq.get("http://0.0.0.0:8081/get");
         assert(rs.code == 200);
     }
 }

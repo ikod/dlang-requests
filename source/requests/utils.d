38dc079b8aeaac162ad3f075a47fc2a585cc1821
Inkrementator <70717315+Inkrementator@users.noreply.github.com>
2026-04-26 21:45:07 +0200

    urlEncoded: Do a first pass with phobos encodeComponent
    Both phobos encodeComponent and this libaries' urlEcoded don't fully
    comply with
    [x-url-formencoded standard by WHATWG](https://url.spec.whatwg.org/#application-x-www-form-urlencoded-percent-encode-set)
    which states:
    > The application/x-www-form-urlencoded percent-encode set contains all code points, except
    > the [ASCII alphanumeric](https://infra.spec.whatwg.org/#ascii-alphanumeric), U+002A (*),
    > U+002D (-), U+002E (.), and U+005F (_).
    
    By combining both, we can get a more correct algorithm
    1. phobos encodes a bunch of non-printable characters that aren't all
       enumerated in this table
    2. `<>\^{}|` is missed by the table but phobos gets them
    3. phobos doesn't encode the `sub-delims` from RFC3986 `!'()*`, which
       x-url-formencoded requires to be percent encoded, which we can patch
       with the table
    
    The character '~' is not encoded by both phobos and this table, the
    change is intentionally omitted here.

--- source/requests/utils.d
+++ source/requests/utils.d
@@ -160,16 +160,20 @@ string[] dump(in ubyte[] data) {
 }
 
 static string urlEncoded(string p) pure @safe {
+    import std.uri: encodeComponent;
     immutable string[dchar] translationTable = [
         ' ':  "%20", '!': "%21", '*': "%2A", '\'': "%27", '(': "%28", ')': "%29",
         ';':  "%3B", ':': "%3A", '@': "%40", '&':  "%26", '=': "%3D", '+': "%2B",
         '$':  "%24", ',': "%2C", '/': "%2F", '?':  "%3F", '#': "%23", '[': "%5B",
-        ']':  "%5D", '%': "%25",
+        ']':  "%5D",
     ];
-    return p.translate(translationTable);
+    return p.encodeComponent().translate(translationTable);
 }
 package unittest {
     assert(urlEncoded(`abc !#$&'()*+,/:;=?@[]`) == "abc%20%21%23%24%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40%5B%5D");
+    assert(urlEncoded("\0.ä.ඞ") == "%00.%C3%A4.%E0%B6%9E");
+    string s = "\0.ä.ඞ";
+    assert(s.urlEncoded().urlDecode() == s);
 }
 
 private static immutable char[string] hex2chr;
